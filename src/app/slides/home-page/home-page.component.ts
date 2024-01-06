81f2dab2029bb1906f778566268ad1037dddb559
izzie1A <47637788+izzie1A@users.noreply.github.com>
2024-01-06 07:27:32 +0000

    navbar pagecontrol ui

--- src/app/slides/home-page/home-page.component.ts
+++ src/app/slides/home-page/home-page.component.ts
@@ -9,21 +9,32 @@ import { Observable } from 'rxjs/internal/Observable';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-  address: string = 'test';
+  address: string = 'items';
   item$: Observable<any[]> = this.fbS.t(this.address);
   itemArray: any[] = [];
   homeArray: any[] = [0,];
 
   constructor(private fbS: FirebaseControlService) {
+    this.fbS.t(this.address).subscribe((r: any[]) => {
+      this.itemArray = r;
+      this.homeArray.push(r[0])
+      console.log(r);
+    })
   }
 
   onScroll() {
+
     console.log("scrolled!!");
+    console.log(this.homeArray.length);
+    console.log(this.itemArray);
+    console.log(this.itemArray[this.homeArray.length]);
     if (this.homeArray.length < 4) {
       if (this.itemArray[this.homeArray.length] != undefined) {
         console.warn(this.itemArray[this.homeArray.length]);
         let x = this.itemArray[this.homeArray.length];
         this.homeArray.push(x);
+        // this.itemArray.push(0);
+        console.log(x);
         console.log(this.homeArray);
       }
     }
